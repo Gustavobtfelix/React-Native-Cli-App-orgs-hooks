3e37f8b288835063fecb7394d89b42c7eb25865e
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-08-22 12:41:44 -0300

    Inicial RN CLI Project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  SafeAreaView, Text } from 'react-native';
+
+function App() {
+  return <>
+    <SafeAreaView>
+      <Text>Alura</Text>
+    </SafeAreaView> 
+  </>
+}
+
+export default App;
