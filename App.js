a3d52253def2e700ceabc42c31c8fbebe6bdc058
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-08-24 14:34:58 -0300

    Revert "Exemplo de uso de classe."
    This reverts commit 0b4a5ba59b263bef78281c3c3dc93ccb03c4c5b7.

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
 import Home from './src/telas/Home/index';
+import mock from './src/mocks/topo'
 
 function App() {
   return <>
     <SafeAreaView>
-      <Home />
+      <Home {...mock}/>
     </SafeAreaView> 
   </>
 }
--- src/telas/Home/Componentes/Topo.js
+++ src/telas/Home/Componentes/Topo.js
@@ -2,36 +2,17 @@ import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 
 import logoOrgsImagem from '../../../../assets/logoOrgs.png';
-import { carregaTopo } from '../../../servicos/carregaDados';
 
 
 
-class Topo extends React.Component{ //extends herda as propriedades de outra classe
-    state = {
-        topo: {
-            boasVindasTXT: '',
-            legendaTXT: '',
-        }
-    }
-
-    atualizaTopo() {
-        const retorno = carregaTopo();
-        this.setState({ topo: retorno }); // setState atualiza as informações se ouver alguma alteração no topo
-    }
-
-    componentDidMount() { //é um metodo que é executado somente uma vêz
-        this.atualizaTopo();        
-    }
-
-    render() { //render é um metodo obrigatorio para criar um componente
-        return <>
-            <View style={estilos.Topo}>
-                <Image source={logoOrgsImagem} style={estilos.logoOrgs}/>
-                <Text style={estilos.BoasVindas}>{this.state.topo.boasVindasTXT}</Text>
-                <Text style={estilos.legenda}>{this.state.topo.legendaTXT}</Text>
-            </View>
-        </>
-    }
+function Topo({boasVindasTXT, legendaTXT}) {
+    return <>
+       <View style={estilos.Topo}>
+       <Image source={logoOrgsImagem} style={estilos.logoOrgs}/>
+       <Text style={estilos.BoasVindas}>{boasVindasTXT}</Text>
+       <Text style={estilos.legenda}>{legendaTXT}</Text>
+       </View>
+    </>
   }
   
   
